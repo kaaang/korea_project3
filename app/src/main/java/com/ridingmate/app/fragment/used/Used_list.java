1fc14f950efc3e80ad005d09fd4e922ffc1d26c5
noobbrother112 <sukho8757@gmail.com>
2021-07-26 12:26:23 +0900

    0726

--- app/src/main/java/com/ridingmate/app/fragment/used/Used_list.java
+++ app/src/main/java/com/ridingmate/app/fragment/used/Used_list.java
@@ -1,18 +1,56 @@
 package com.ridingmate.app.fragment.used;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.ridingmate.app.R;
+import com.ridingmate.app.util.used.UsedListAdapter;
+import com.ridingmate.app.util.used.UsedListData;
+
+import java.util.ArrayList;
 
 public class Used_list extends Fragment {
+
+    private LinearLayoutManager linearLayoutManager;
+    private RecyclerView recyclerView;
+    private ArrayList<UsedListData> arrayList;
+    private UsedListAdapter usedListAdapter;
+
     @Override
     public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_used_list,container,false);
+
+
+
+        recyclerView=(RecyclerView)view.findViewById(R.id.used_RV);
+        linearLayoutManager= new LinearLayoutManager(view.getContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        arrayList=new ArrayList<>();
+
+        usedListAdapter=new UsedListAdapter(arrayList);
+        recyclerView.setAdapter(usedListAdapter);
+
+        for (int i=0;i<=30;i++){
+
+        UsedListData usedListData = new UsedListData(R.mipmap.ic_launcher,"","신혀기 람브동생 커브팔아여","커브","9","975");
+        arrayList.add(usedListData);
+        }
+
+        usedListAdapter.notifyDataSetChanged();
+
+
+
+
+
+
         return view;
     }
 }
---
+++ app/src/main/java/com/ridingmate/app/util/used/UsedListData.java
@@ -0,0 +1,67 @@
+package com.ridingmate.app.util.used;
+
+public class UsedListData {
+    private int used_thumb;
+    private String used_thumbSrc;
+    private String used_title;
+    private String model_type;
+    private String used_comment_cont;
+    private String used_welth;
+
+    public UsedListData(int used_thumb, String used_thumbSrc, String used_title, String model_type, String used_comment_cont, String used_welth) {
+        this.used_thumb = used_thumb;
+        this.used_thumbSrc = used_thumbSrc;
+        this.used_title = used_title;
+        this.model_type = model_type;
+        this.used_comment_cont = used_comment_cont;
+        this.used_welth = used_welth;
+    }
+
+    public int getUsed_thumb() {
+        return used_thumb;
+    }
+
+    public void setUsed_thumb(int used_thumb) {
+        this.used_thumb = used_thumb;
+    }
+
+    public String getUsed_thumbSrc() {
+        return used_thumbSrc;
+    }
+
+    public void setUsed_thumbSrc(String used_thumbSrc) {
+        this.used_thumbSrc = used_thumbSrc;
+    }
+
+    public String getUsed_title() {
+        return used_title;
+    }
+
+    public void setUsed_title(String used_title) {
+        this.used_title = used_title;
+    }
+
+    public String getModel_type() {
+        return model_type;
+    }
+
+    public void setModel_type(String model_type) {
+        this.model_type = model_type;
+    }
+
+    public String getUsed_comment_cont() {
+        return used_comment_cont;
+    }
+
+    public void setUsed_comment_cont(String used_comment_cont) {
+        this.used_comment_cont = used_comment_cont;
+    }
+
+    public String getUsed_welth() {
+        return used_welth;
+    }
+
+    public void setUsed_welth(String used_welth) {
+        this.used_welth = used_welth;
+    }
+}

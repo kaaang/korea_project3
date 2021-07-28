f40b533a2da9b5a682bc8bcb42deba328e7d370d
noobbrother112 <sukho8757@gmail.com>
2021-07-28 22:07:45 +0900

    0728

---
+++ app/src/main/java/com/ridingmate/app/util/used/UsedWriteData.java
@@ -0,0 +1,17 @@
+package com.ridingmate.app.util.used;
+
+public class UsedWriteData {
+    private int used_write_thumb;
+
+    public UsedWriteData(int used_write_thumb) {
+        this.used_write_thumb = used_write_thumb;
+    }
+
+    public int getUsed_write_thumb() {
+        return used_write_thumb;
+    }
+
+    public void setUsed_write_thumb(int used_write_thumb) {
+        this.used_write_thumb = used_write_thumb;
+    }
+}

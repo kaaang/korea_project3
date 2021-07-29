4880ede5d1671594bf3ef5412a5149d6ddcd4390
Katherine1212 <katherinekim971212@gmail.com>
2021-07-29 20:21:53 +0900

    maintenace // 0729 Jinah

--- app/src/main/java/com/ridingmate/app/fragment/main/Main_maintenance_list.java
+++ app/src/main/java/com/ridingmate/app/fragment/main/Main_maintenance_list.java
@@ -6,13 +6,43 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.ridingmate.app.R;
+import com.ridingmate.app.util.maintenance.MaintenanceAdpter;
+import com.ridingmate.app.util.maintenance.MaintenanceDAO;
+
+import java.util.ArrayList;
 
 public class Main_maintenance_list extends Fragment {
-    @Override
+    private LinearLayoutManager linearLayoutManager;
+    private RecyclerView recyclerView;
+    private ArrayList<MaintenanceDAO> arrayList;
+    private MaintenanceAdpter adpter;
+
     public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_main_maintenance_list,container,false);
+
+        linearLayoutManager= new LinearLayoutManager((view.getContext()));
+        recyclerView= (RecyclerView)view.findViewById(R.id.maintentance_list);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.addItemDecoration(new DividerItemDecoration(view.getContext(), 1));
+
+        arrayList= new ArrayList<>();
+        adpter= new MaintenanceAdpter(arrayList);
+        recyclerView.setAdapter(adpter);
+
+        /* 주유 기록까지 넘겨 받아서 캘린더와 정비기록란에 같이 띄우고 싶음.*/
+
+        for(int i=0; i<10; i++){
+            MaintenanceDAO dao= new MaintenanceDAO("엔진오일","2021.07.28", "S오일 송파점");
+            arrayList.add(dao);
+        }
+        adpter.notifyDataSetChanged();
         return view;
+
+
     }
 }
---
+++ app/src/main/java/com/ridingmate/app/util/maintenance/MaintenanceAdpter.java
@@ -0,0 +1,65 @@
+package com.ridingmate.app.util.maintenance;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ridingmate.app.R;;
+
+import java.util.ArrayList;
+
+
+public class MaintenanceAdpter extends RecyclerView.Adapter<MaintenanceAdpter.CustomViewHolder> {
+    private ArrayList<MaintenanceDAO> arrayList;
+
+    public MaintenanceAdpter(ArrayList<MaintenanceDAO> arrayList){
+        this.arrayList=arrayList;
+    }
+    public CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_main_maintenance_list,parent,false);
+
+        CustomViewHolder holder=new CustomViewHolder(view);
+
+
+        return holder;
+    }
+
+    public void onBindViewHolder(MaintenanceAdpter.CustomViewHolder holder, int position) {
+
+        holder.item_maintenance.setText(arrayList.get(position).getItem_maintenance());
+        holder.item_maintenance_date.setText(arrayList.get(position).getItem_maintenance_date());
+        holder.item_maintenance_location.setText(arrayList.get(position).getItem_maintenance_location());
+
+        holder.itemView.setTag(position);
+        holder.itemView.setOnClickListener(v -> {
+            String title= holder.item_maintenance.getText().toString();
+            Toast.makeText(v.getContext(),title,Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    public int getItemCount() {
+        return (null != arrayList ? arrayList.size():0);
+    }
+
+    public class CustomViewHolder extends RecyclerView.ViewHolder{
+        public TextView item_maintenance;
+        public TextView item_maintenance_date;
+        public TextView item_maintenance_location;
+
+
+        public CustomViewHolder(View view) {
+            super(view);
+
+            this.item_maintenance = (TextView)view.findViewById(R.id.item_maintenance);
+            this.item_maintenance_date = (TextView)view.findViewById(R.id.item_maintenance_date);
+            this.item_maintenance_location = (TextView)view.findViewById(R.id.item_maintenance_location);
+
+
+
+        }
+    }
+}
